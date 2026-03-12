665c44cfecb9bb3173b375ad601bd90ca82d1346
Khushi Dixit <120947499+kskd16@users.noreply.github.com>
2026-03-12 14:56:43 +0530

    changes

--- Week 5/book-service/src/main/java/com/lpu/book_service/repo/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lpu.book_service.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lpu.book_service.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- Week 5/order-service/src/main/java/com/lpu/order_service/repo/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lpu.order_service.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lpu.order_service.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
